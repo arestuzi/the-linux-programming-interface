d170999d5671290e461370bd99c5675db78fe801
Binglin Ji <cat3062485@hotmail.com>
2017-08-31 21:17:47 +0800

    first program for fileio

--- lib/tlpi_hdr.h
+++ lib/tlpi_hdr.h
@@ -5,3 +5,18 @@
 #include <stdio.h>      /* Standard I/O functions */
 #include <stdlib.h>     /* Prototypes of commonly used library functions,
                            plus EXIT_SUCCESS and EXIT_FAILURE constants */
+#include <unistd.h>     /* Prototypes for many system calls */
+#include <errno.h>      /* Declares errno and defines error constants */
+#include <string.h>     /* Commonly used string-handling functions */
+
+#include "get_num.h"    /* Declares our functions for handling numberic
+                           arguments (getInt(), getLong()) */
+
+#include "error_functions.h"    /* Declares our error-handling functions */
+
+typedef enum { FALSE, TRUE } Boolean;
+
+#define min(m,n) ((m) < (n) ? (m) : (n))
+#define max(m,n) ((m) > (n) ? (m) : (n))
+
+#endif
