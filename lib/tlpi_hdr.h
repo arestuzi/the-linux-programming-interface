4d0f719611725fc65a65f04cb88f9890e7efab39
root <root@vm218-106.vm.gsslab.pek.redhat.com>
2017-08-31 16:01:01 +0800

    init send

---
+++ lib/tlpi_hdr.h
@@ -0,0 +1,7 @@
+#ifndef TLPI_HDR_H
+#define TLPI_HDR_H      /* Prevent accidental double inclusion */
+
+#include <sys/types.h>  /* Type definitions used by many programs */
+#include <stdio.h>      /* Standard I/O functions */
+#include <stdlib.h>     /* Prototypes of commonly used library functions,
+                           plus EXIT_SUCCESS and EXIT_FAILURE constants */
